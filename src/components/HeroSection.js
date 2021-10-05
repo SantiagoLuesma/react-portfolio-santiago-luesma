90a23f8869f3005593c04f8acd85f3b5e851f2e5
Luesma <seluesma@gmail.com>
2021-10-04 23:47:53 -0300

    style(frontend): Agrego estilo a sección principal
    En este commit le doy estilos a la sección principal (Home)

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,70 +1,221 @@
 import React from 'react';
+import styled from 'styled-components';
 // import HeroImg from '../assets/images/santiagoluesma-width.jpg';
 import Button from './Button';
 import PText from './PText';
 import SocialMediaArrow from '../assets/images/social-media-arrow.svg';
 import scrollDownArrow from '../assets/images/scroll-down-arrow.svg';
 
+const HeroStyles = styled.div`
+  .hero {
+    height: 100vh;
+    min-height: 1000px;
+    width: 100%;
+    text-align: center;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+  }
+
+  .hero__heading {
+    font-size: 2rem;
+    margin-bottom: -4rem;
+    position: relative;
+    span {
+      display: inline-block;
+      width: 100%;
+    }
+    .hero__name {
+      font-size: 7rem;
+      font-family: 'Montserrat SemiBold';
+      color: var(--white);
+    }
+  }
+  .hero__img {
+    max-width: 900px;
+    width: 100%;
+    height: 600px;
+    margin: 0 auto;
+    border: 2px solid var(--gray-1);
+  }
+  .hero__info {
+    margin-top: -18rem;
+  }
+  .hero__social,
+  .hero__scrollDown {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    position: absolute;
+    bottom: 20px;
+    width: 50px;
+  }
+  .hero__social {
+    left: 50px;
+  }
+  .hero__scrollDown {
+    right: 50px;
+  }
+  .hero__social__indicator,
+  .hero__scrollDown {
+    width: 50px;
+    p {
+      font-size: 1.6rem;
+      transform: translateY(-70px) rotate(90deg);
+      letter-spacing: 0.7rem;
+      text-transform: uppercase;
+    }
+    img {
+      max-height: 45px;
+      width: 16px;
+      margin: 0 auto;
+      object-fit: contain;
+    }
+  }
+  .hero__scrollDown {
+    img {
+      max-height: 70px;
+    }
+  }
+  .hero__social__text {
+    ul {
+      li {
+        margin-bottom: 1rem;
+      }
+      a {
+        display: inline-block;
+        font-size: 1.6rem;
+        transform: rotate(-90deg);
+        letter-spacing: 5px;
+        margin-bottom: 2rem;
+      }
+    }
+  }
+  @media only screen and (max-width: 768px) {
+    .hero {
+      min-height: 750px;
+    }
+    .hero__heading {
+      font-size: 1.4rem;
+      margin-bottom: -3rem;
+      .hero__name {
+        font-size: 4.5rem;
+      }
+    }
+    .hero__img {
+      height: 300px;
+    }
+    .hero__info {
+      margin-top: 3rem;
+    }
+    .hero__social {
+      left: 0px;
+      bottom: 15%;
+      width: 20px;
+      .hero__social__indicator {
+        width: 20px;
+        p {
+          font-size: 1.2rem;
+        }
+        img {
+          max-height: 22px;
+        }
+      }
+      .hero__social__text {
+        ul {
+          li {
+            a {
+              font-size: 1.2rem;
+              margin-bottom: 1rem;
+            }
+          }
+        }
+      }
+    }
+    .hero__scrollDown {
+      right: 0;
+      width: 20px;
+      gap: 1rem;
+      p {
+        font-size: 1.3rem;
+      }
+    }
+  }
+`;
+
 export default function HeroSection() {
   return (
-    <div>
-      <div className="container">
-        <h1 className="hero__heading">
-          <span>Hola, yo soy</span>
-          <span>Santiago Luesma</span>
-        </h1>
-        {/* <div className="hero__img">
-          <img src={HeroImg} alt="" />
-        </div> */}
-        <div className="hero__info">
-          <PText>
-            I am working as a freelance web designer and developer for 4 years.
-            I love to design and make new web experiences for the people.
-          </PText>
-          <Button btnLink="/projects" btnText="see my works" />
-        </div>
-        <div className="hero__social">
-          <div className="hero__social__indicator">
-            <p>Follow</p>
-            <img src={SocialMediaArrow} alt="social media arrow" />
+    <HeroStyles>
+      <div className="hero">
+        <div className="container">
+          <h1 className="hero__heading">
+            <span>Hola, yo soy</span>
+            <span className="hero__name">Santiago Luesma</span>
+          </h1>
+          <div className="hero__img">
+            <img src="https://picsum.photos/id/1059/970/608?grayscale" alt="" />
           </div>
-          <div className="hero__social__text">
-            <ul>
-              <li>
-                <a
-                  href="https://twitter.com/luesmadev"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  TW
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://github.com/santiagoluesma"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  GH
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://linkedin.com/in/santiagoluesma"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  LI
-                </a>
-              </li>
-            </ul>
+          <div className="hero__info">
+            <PText>
+              I am working as a freelance web designer and developer for 4
+              years. I love to design and make new web experiences for the
+              people.
+            </PText>
+            <Button btnLink="/projects" btnText="see my works" />
           </div>
-        </div>
-        <div className="hero__scrollDown">
-          <p>Scroll</p>
-          <img src={scrollDownArrow} alt="" />
+          <div className="hero__social">
+            <div className="hero__social__indicator">
+              <p>Follow</p>
+              <img src={SocialMediaArrow} alt="social media arrow" />
+            </div>
+            <div className="hero__social__text">
+              <ul>
+                <li>
+                  <a
+                    href="https://github.com/santiagoluesma"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    GH
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://linkedin.com/in/santiagoluesma"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    LI
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://twitter.com/luesmadev"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    TW
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://instagram.com/santiagoluesma"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    IG
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="hero__scrollDown">
+            <p>Scroll</p>
+            <img src={scrollDownArrow} alt="" />
+          </div>
         </div>
       </div>
-    </div>
+    </HeroStyles>
   );
 }
