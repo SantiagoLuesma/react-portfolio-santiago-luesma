accb3ef0c5c64613bf1033d4f8153369d62a0076
Luesma <seluesma@gmail.com>
2021-10-07 17:01:50 -0300

    style(frontend): Agrego estilo a sección Proyectos
    En este commit le di estilos a la sección de Proyectos y también lo adapte
    para los distintos tamaños de pantalla1 (responsive design).

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,21 +1,57 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 import projectImg from '../assets/images/projectImg.png';
 
-export default function ProjectItem() {
+const ProjectItemStyles = styled.div`
+  .projectItem__img {
+    width: 100%;
+    height: 400px;
+    overflow: hidden;
+    border-radius: 12px;
+    display: inline-block;
+    border: 3px solid var(--gray-2);
+    img {
+      height: 100%;
+    }
+  }
+  .projectItem__info {
+    margin-top: 1rem;
+    background-color: var(--deep-dark);
+    padding: 1rem;
+    border-radius: 12px;
+  }
+  .projectItem__title {
+    font-size: 2.2rem;
+  }
+  .projectItem__desc {
+    font-size: 1.6rem;
+    font-family: 'RobotoMono Regular';
+    margin-top: 1rem;
+  }
+  @media only screen and (max-width: 768px) {
+    .projectItem__img {
+      height: 350px;
+    }
+  }
+`;
+
+export default function ProjectItem({
+  img = projectImg,
+  title = 'Project Name',
+  desc = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit odio, unde.',
+}) {
   return (
-    <div>
+    <ProjectItemStyles>
       <Link to="/projects" className="projectItem__img">
-        <img src={projectImg} alt="Project Img" />
+        <img src={img} alt="Project Img" />
       </Link>
       <div className="projectItem__info">
         <Link to="#">
-          <h3 className="projectItem__title">Project 1</h3>
+          <h3 className="projectItem__title">{title}</h3>
         </Link>
-        <p className="projectItem__desc">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, odio, unde.
-        </p>
+        <p className="projectItem__desc">{desc}</p>
       </div>
-    </div>
+    </ProjectItemStyles>
   );
 }
