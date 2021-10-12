5e1c59bd496d0589b0030661f0e33c2c162e6361
Luesma <seluesma@gmail.com>
2021-10-12 17:37:31 -0300

    style(frontend): Doy estilo a sección footer
    En este commit le doy estilos a la sección de footer del home page.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import styled from 'styled-components';
 import FooterCol from './FooterCol';
 import PText from './PText';
 
+const FooterStyles = styled.div`
+  padding-top: 10rem;
+  background-color: var(--deep-dark);
+  .container {
+    display: flex;
+    gap: 3rem;
+  }
+  .footer__col1 {
+    flex: 2;
+  }
+  .footer__col2,
+  .footer__col3,
+  .footer__col4 {
+    flex: 1;
+  }
+  .footer__col1__title {
+    font-size: 3.5rem;
+    margin-bottom: 1rem;
+  }
+`;
+
 export default function Footer() {
   return (
-    <div>
+    <FooterStyles>
       <div className="container">
         <div className="footer__col1">
           <h1 className="footer__col1__title">Santiago Luesma</h1>
@@ -22,6 +44,6 @@ export default function Footer() {
           <FooterCol />
         </div>
       </div>
-    </div>
+    </FooterStyles>
   );
 }
