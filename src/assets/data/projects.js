a156cd4f37af663876aab4d685ec5073656b4bad
Luesma <seluesma@gmail.com>
2021-10-18 00:52:35 -0300

    build(frontend): Creo la página de Proyectos
    En este commit creo la página de proyectos y agrego cada uno de los mismos en él.

--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -1,45 +1,28 @@
 import { v4 as uuidv4 } from 'uuid';
-import ProjectImg from '../images/projectImg.png';
 import UTrackerImg from '../images/utracker.jpg';
 import GreenCtgImg from '../images/greenctg.jpg';
 import CoinTrackerImg from '../images/cointracker.jpg';
-import CavinImg from '../images/cavinimg.jpg';
 
 const projects = [
   {
     id: uuidv4(),
-    name: 'U Tracker',
+    name: 'CRUD Tasks',
     desc:
-      'Una aplicación para rastrear todos sus datos desde un solo lugar. Desarrollé el sitio web y la aplicación móvil.',
+      'CRUD de proyectos y tareas, creado con la librería React de JavaScript.',
     img: UTrackerImg,
   },
   {
     id: uuidv4(),
-    name: 'Green CTG',
+    name: 'Weather App',
     desc:
-      'Una aplicación para ayudar a las personas a obtener una visión general de cómo pueden hacer que la ciudad sea hermosa.',
+      'Aplicación del tiempo utilizando HTML, CSS y JavaScript, utilizando una API pública para extraer los datos.',
     img: GreenCtgImg,
   },
   {
     id: uuidv4(),
-    name: 'Coin Tracker',
-    desc:
-      'Con esta aplicación puede rastrear cualquier moneda electrónica. También recibirá un buen asesoramiento sobre inversiones por parte del profesional.',
+    name: 'Calculadora',
+    desc: 'Calculadora creada con HTML, CSS Y JavaScript Vanilla.',
     img: CoinTrackerImg,
-  },
-  {
-    id: uuidv4(),
-    name: "Cavin's Portfolio",
-    desc:
-      'Un portafolio para Cavin jr. Un artista de la ciudad de Nueva York. La cartera se realizó utilizando ReactJs y GatsbyJs.',
-    img: CavinImg,
-  },
-  {
-    id: uuidv4(),
-    name: 'Tracking Soft',
-    desc:
-      'Un sitio web de seguimiento que mostrará el rendimiento del sitio web. También obtendrás algunos consejos útiles para mejorar el rendimiento.',
-    img: ProjectImg,
   },
 ];
 
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { MdSearch } from 'react-icons/md';
+import SectionTitle from '../components/SectionTitle';
+import ProjectItem from '../components/ProjectItem';
+import ProjectInfo from '../assets/data/projects';
 
 export default function Projects() {
+  const [projectData, setProjectData] = useState(ProjectInfo);
+
   return (
     <div>
-      <h1>Desde Projects</h1>
+      <div className="container">
+        <SectionTitle heading="Projects" subheading="some of my recent works" />
+        <div className="projects__searchbar">
+          <form>
+            <input type="text" />
+            <MdSearch />
+          </form>
+        </div>
+        <div className="projects__allItems">
+          {projectData.map((item) => (
+            <ProjectItem
+              key={item.id}
+              title={item.name}
+              desc={item.desc}
+              img={item.img}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
