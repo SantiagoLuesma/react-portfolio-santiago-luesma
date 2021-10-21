ffb4088505c8e4b081a9599df17d71e9f71250bc
Luesma <seluesma@gmail.com>
2021-10-21 04:54:41 -0300

    build(frontend): Modifico la barra de scroll
    Modifico el estilo y el movimiento de la barra de scroll, también modifico
    que al cambiar de página el scroll vuelva a la parte de arriba de la página

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,9 +1,49 @@
 import React from 'react';
+import styled from 'styled-components';
 import PText from './PText';
+import MapImg from '../assets/images/map-cba.png';
+
+const MapStyles = styled.div`
+  background: url(${MapImg}) no-repeat center / cover;
+  min-height: 400px;
+  .container {
+    position: relative;
+    min-height: 400px;
+  }
+  .map__card {
+    position: absolute;
+    right: 10%;
+    bottom: 10%;
+    padding: 2rem;
+    background: var(--deep-dark);
+    width: 100%;
+    max-width: 300px;
+    border-radius: 12px;
+  }
+  .map__card__heading {
+    font-size: 3rem;
+    margin-bottom: 1rem;
+  }
+  .map__card__link {
+    display: inline-block;
+    font-size: 1.6rem;
+    margin-top: 3rem;
+    text-decoration: underline;
+  }
+  @media only screen and (max-width: 768px) {
+    background-position: 80% center;
+  }
+  @media only screen and (max-width: 400px) {
+    .map__card {
+      max-width: none;
+      right: auto;
+    }
+  }
+`;
 
 export default function Map() {
   return (
-    <div>
+    <MapStyles>
       <div className="container">
         <div className="map__card">
           <h3 className="map__card__heading">Here is me</h3>
@@ -12,11 +52,12 @@ export default function Map() {
             href="https://www.google.com.ar/maps/place/Centro,+C%C3%B3rdoba/@-31.4150309,-64.1904697,15z/data=!3m1!4b1!4m5!3m4!1s0x9432a29d1d3f9d45:0xcbcd910412819792!8m2!3d-31.4164315!4d-64.1827297"
             target="_blank"
             rel="noreferrer"
+            className="map__card__link"
           >
             Open in Google Map
           </a>
         </div>
       </div>
-    </div>
+    </MapStyles>
   );
 }
